9d353648dfb70f35b1171f93809b0312a49b92e0
Marcio Maciel <marcio.maciel@evino.com.br>
2021-06-12 17:35:23 -0300

    build script

--- build-all.sh
+++ build-all.sh
@@ -5,9 +5,10 @@ docker build -t dockerisioner/base:"$VERSION" --no-cache base/
 docker push dockerisioner/base:"$VERSION"
 docker build -t dockerisioner/base-app:"$VERSION" --no-cache base-app/
 docker push dockerisioner/base-app:"$VERSION"
-docker build -t dockerisioner/php:7.2 --no-cache php/7.2/
+docker build -t dockerisioner/php:7.4 --no-cache php/7.4/
 docker push dockerisioner/php:7.4
-docker build -t dockerisioner/php-nginx:7.2 --no-cache php-nginx/7.2/
+docker build -t dockerisioner/php-nginx:7.4 --no-cache php-nginx/7.4/
+docker build -t dockerisioner/php-nginx:7.4 --no-cache php-nginx/7.4/
 docker push dockerisioner/php-nginx:7.4
 docker build -t evinobr/magento-base:v2.4 --no-cache magento-base/
 docker push evinobr/magento-base:v2.4
